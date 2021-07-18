1500da621c0ba565fa9bcbd492ce431e97bb3eea
J2mF <mp4-12cs5@hotmail.com>
2021-07-18 20:44:23 +0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul>
+        <li v-for="resource in storedResources"></li>
+    </ul>
+</template>
+
+<script>
+    export default {
+      data() {
+        return {
+          storedResources: [
+            {
+              id: 1,
+              title: 'Official Guide',
+              description: 'The official Vue.js documentation',
+              link: 'https://vuejs.org',
+            },
+            {
+              id: 2,
+              title: 'Google',
+              description: 'Learn to google',
+              link: 'https://google.com',
+            },
+          ],
+        }
+      }
+    }
+</script>

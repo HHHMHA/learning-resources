41283b3e77276f8d0cd39d3eb6bd9f5d83f42a46
J2mF <mp4-12cs5@hotmail.com>
2021-08-01 20:59:50 +0300

    Add StoredResources Component
    Reformat File

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,45 @@
 <template>
-    <ul>
-        <learning-resource v-for="resource in storedResources"
-                           :key="resource.id"
-                           :title="resource.title"
-                           :description="resource.description"
-                           :link="resource.link"
-        />
-    </ul>
+    <StoredResources :resources='resources' />
 </template>
 
 <script>
-  import LearningResource from './components/LearningResource';
+import StoredResources from '@/StoredResources';
 
-  export default {
-      components: { LearningResource },
-      data() {
+export default {
+    components: { StoredResources},
+    data() {
         return {
-          storedResources: [
-            {
-              id: 1,
-              title: 'Official Guide',
-              description: 'The official Vue.js documentation',
-              link: 'https://vuejs.org',
-            },
-            {
-              id: 2,
-              title: 'Google',
-              description: 'Learn to google',
-              link: 'https://google.com',
-            },
-          ],
-        }
-      }
+            resources: [
+                {
+                    id: 1,
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 2,
+                    title: 'Google',
+                    description: 'Learn to google',
+                    link: 'https://google.com'
+                }
+            ]
+        };
     }
+};
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
---
+++ src/StoredResources.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul>
+        <learning-resource v-for='resource in resources'
+                           :key='resource.id'
+                           :title='resource.title'
+                           :description='resource.description'
+                           :link='resource.link'
+        />
+    </ul>
+</template>
+
+<script>
+import LearningResource from './components/LearningResource';
+
+export default {
+    name: 'StoredResources',
+    components: { LearningResource },
+    props: ['resources']
+};
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    padding: 0;
+    margin: auto;
+    max-width: 40rem;
+}
+</style>
